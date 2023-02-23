4db61589086771649639e677a0a2eba2c07e74ba
Aaditya Pal <aadityap@trainee.nrifintech.com>
2023-02-23 11:05:09 +0530

    Initial commit

---
+++ src/main/java/nrifintech/busMangementSystem/BusMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package nrifintech.busMangementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusMangementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusMangementSystemApplication.class, args);
+	}
+
+}
