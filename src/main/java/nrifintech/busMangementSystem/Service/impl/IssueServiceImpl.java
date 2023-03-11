b5402773a1140cf62360da6e8f9500aa45fcf769
Aaditya Pal <aadityap@trainee.nrifintech.com>
2023-03-11 09:01:43 +0530

    Issue services

---
+++ src/main/java/nrifintech/busMangementSystem/Service/impl/IssueServiceImpl.java
@@ -0,0 +1,55 @@
+package nrifintech.busMangementSystem.Service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import nrifintech.busMangementSystem.Service.interfaces.DestinationService;
+import nrifintech.busMangementSystem.Service.interfaces.IssueService;
+import nrifintech.busMangementSystem.entities.Destination;
+import nrifintech.busMangementSystem.entities.Issue;
+import nrifintech.busMangementSystem.exception.ResouceNotFound;
+
+import nrifintech.busMangementSystem.exception.UnauthorizedAction;
+
+
+import nrifintech.busMangementSystem.repositories.DestinationRepo;
+import nrifintech.busMangementSystem.repositories.IssueRepo;
+
+
+@Service
+public class IssueServiceImpl implements IssueService {
+
+    @Autowired
+    IssueRepo issueRepo;
+
+    @Override
+    public List<Issue> getAllunResolvedIssue() {
+       return issueRepo.getAllunResolvedIssue();
+    }
+
+    @Override
+    public List<Issue> getIssuesByUserId(int userId) {
+        return issueRepo.getIssuesByUserId(userId);
+    }
+
+    @Override
+    public void addIssue(Issue issue){
+        issueRepo.save(issue);
+    }
+
+    @Override
+    public void resolveIssue(int issueId){
+        Optional<Issue> _issue = issueRepo.findById(issueId);
+        Issue issue = _issue.get();
+
+        issue.setIsResolved(1);
+        issueRepo.save(issue);
+    }
+
+	
+
+}
---
+++ src/main/java/nrifintech/busMangementSystem/Service/interfaces/IssueService.java
@@ -0,0 +1,19 @@
+package nrifintech.busMangementSystem.Service.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import nrifintech.busMangementSystem.entities.Bus;
+import nrifintech.busMangementSystem.entities.Issue;
+import nrifintech.busMangementSystem.entities.Route;
+import nrifintech.busMangementSystem.entities.RouteInfo;
+
+@Service
+public interface IssueService {
+	List<Issue> getAllunResolvedIssue();
+    List<Issue> getIssuesByUserId(int userId);
+    void addIssue(Issue issue);
+    void resolveIssue(int issueId);
+}
