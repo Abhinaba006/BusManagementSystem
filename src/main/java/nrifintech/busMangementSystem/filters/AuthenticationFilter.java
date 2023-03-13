3c9f4583972eb313c8a10f23eca671a673bcbd3d
Abhinaba <abhinabad@trainee.nrifintech.com>
2023-03-13 13:49:27 +0530

    differnt admin permission added depending on role

--- src/main/java/nrifintech/busMangementSystem/filters/AuthenticationFilter.java
+++ src/main/java/nrifintech/busMangementSystem/filters/AuthenticationFilter.java
@@ -21,58 +21,63 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 
 	@Autowired
 	private UserRepo userRepo;
-	
+
 	@Autowired
 	JwtTokenUtil jwtTokenUtil;
-	
+
 	@Autowired
 	UserService userService;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
+
 		// Get the custom header from the request
 //		 String authorizationHeader  = request.getHeader("x-auth-token");
-		String authorizationHeader  = request.getHeader("Authorization");
-	     System.out.println("\n--------------------------------\n----------------------\n");
-	     System.out.println("authorizationHeader: "+authorizationHeader);
-	     System.out.println("\n--------------------------------\n----------------------\n");
-	     
-	     
-		 String url = request.getRequestURI();
-		 if (url.contains("login")) {
-		 	filterChain.doFilter(request, response);
-		 	return;
-		 }
-		 
-	     String token = authorizationHeader;
-	    // // Use the token for authentication/authorization purposes
-	     System.out.println("\n--------------------------------\n----------------------\n");
-		 String payload = jwtTokenUtil.extractUsername(token);
-		 int userId = Integer.parseInt(payload);			 
-		 int type = jwtTokenUtil.extractType(token);
-		 System.out.println(userId+" "+type);
-		 if (url.contains("create") && type==0) {
-			 throw new UnauthorizedAction("Admin permission needed", "undefiened user");
-		 }
-		 System.out.println(userId);
-		 System.out.println("\n--------------------------------\n----------------------\n");
-		 
-		// System.out.println(authorizationHeader);
-		if (userService.getUser(userId) != null) {
-			// if(userRepo.findById(userId).isEmpty()) throw new UnauthorizedAction("invalid credentials", "undefiened user");
+		String authorizationHeader = request.getHeader("Authorization");
+		System.out.println("\n--------------------------------\n----------------------\n");
+		System.out.println("authorizationHeader: " + authorizationHeader);
+		System.out.println("\n--------------------------------\n----------------------\n");
+
+		String url = request.getRequestURI();
+		if (url.contains("login")) {
 			filterChain.doFilter(request, response);
 			return;
 		}
+
+		String token = authorizationHeader;
+		// // Use the token for authentication/authorization purposes
+		System.out.println("\n--------------------------------\n----------------------\n");
+		String payload = jwtTokenUtil.extractUsername(token);
+		int userId = Integer.parseInt(payload);
+		int type = jwtTokenUtil.extractType(token);
+		System.out.println(userId + " " + type);
+		if (userService.getUser(userId) != null) {
+			// if(userRepo.findById(userId).isEmpty()) throw new UnauthorizedAction("invalid
+			// credentials", "undefiened user");
+			filterChain.doFilter(request, response);
+			return;
+		}
+		if (url.contains("create") && type == 0) {
+			throw new UnauthorizedAction("Admin permission needed", "undefiened user");
+		}
+		System.out.println(userId);
+		System.out.println("\n--------------------------------\n----------------------\n");
+
+		// System.out.println(authorizationHeader);
+
 //		System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK");
 //		if (url.contains("/admin"))
 //			userType = 1;
 
-		/*if ((userType == 0 && !userService.checkUser(userName, password)))
-			throw new UnauthorizedAction("Missing or invalid Custom-Header header", "undefiened user");
-		if ((userType == 1 && !userService.checkAdmin(userName, password)))
-			throw new UnauthorizedAction("Missing or invalid Custom-Header header", "undefiened user");*/
+		/*
+		 * if ((userType == 0 && !userService.checkUser(userName, password))) throw new
+		 * UnauthorizedAction("Missing or invalid Custom-Header header",
+		 * "undefiened user"); if ((userType == 1 && !userService.checkAdmin(userName,
+		 * password))) throw new
+		 * UnauthorizedAction("Missing or invalid Custom-Header header",
+		 * "undefiened user");
+		 */
 
 		throw new UnauthorizedAction("Invalid token provided", "undefiened user");
 	}
